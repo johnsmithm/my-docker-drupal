d4f6f746614f3e50f2140074b51556d8eba1b427
johnsmithm <moshnoi2000@gmail.com>
2016-07-31 18:05:32 +0200

    added parameters in the run command

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ docker build -t v1.1 .
 docker stop v1.1
 docker rm v1.1
 #docker run -td -p 8009:80 -v /tmp/data:/var/www/html  --name  v1.1 v1.1
-docker run -td -p 8003:80 -e "MYSQL_ROOT_PASSWORD=password" -v /tmp/data:/var/www/html --name v1.1 v1.1 
+docker run -td -p 8003:80 -e "MYSQL_ROOT_PASSWORD=psw"  -e "MYSQL_USER=username"  -e "MYSQL_DATABASE=db"  -e "MYSQL_HOST=host"  -v /tmp/data:/var/www/html --name v1.1 v1.1 
 docker logs -f v1.1
 #docker run -ti v1.1 /bin/bash
--- start.sh
+++ start.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
 echo "01. setup apache"
-  mkdir /var/log/apache2 2>/dev/null
-  chown -R www-data /var/log/apache2 2>/dev/null
-  a2enmod rewrite vhost_alias headers
-  a2ensite 000-default
+mkdir /var/log/apache2 2>/dev/null
+chown -R www-data /var/log/apache2 2>/dev/null
+a2enmod rewrite vhost_alias headers
+a2ensite 000-default
 
-  mkdir -p /var/lib/drupal-private
-  chown -R www-data /var/lib/drupal-private
+mkdir -p /var/lib/drupal-private
+chown -R www-data /var/lib/drupal-private
 
-  	GIT_REPO="https://github.com/johnsmithm/drupal.the.docker.git"
-  	MYSQL_HOST="192.168.148.199"
-    MYSQL_USER="root"
-    MYSQL_DATABASE="the1-reservation"
-    #MYSQL_ROOT_PASSWORD="password1"
-    MYSQL_PASSWORD="1"
+GIT_REPO="https://github.com/johnsmithm/drupal.the.docker.git"
+#MYSQL_HOST=""
+#MYSQL_USER=""
+#MYSQL_DATABASE=""
+#MYSQL_ROOT_PASSWORD=""
+MYSQL_PASSWORD="1"
 
-  cd /var/www
- rm -rf html
+#todo: handle existing repository
+
 echo "02. cloning repo"
-  git clone ${GIT_REPO} html
-  cd html
+cd /var/www
+rm -rf html
+git clone ${GIT_REPO} html
+cd html
 mkdir -p sites/default/files && chmod 755 sites/default && chown -R www-data:www-data sites/default/files;
+
+echo "03. setting database"
 mv /tmp/settings.php sites/default/settings.php
 sed -i "s/placeholder_PWD/${MYSQL_ROOT_PASSWORD}/g" sites/default/settings.php
 sed -i "s/placeholder_DB/${MYSQL_DATABASE}/g" sites/default/settings.php
@@ -39,7 +43,7 @@ tail -f $webfactlog &
 echo "-- starting /etc/rc.local"
 /etc/rc.local &
 
-echo "10. Starting processes via supervisor."
+echo "4. Starting processes via supervisor."
 # Start lamp, make sure no PIDs lying around
 rm /var/run/apache2/apache2.pid /var/run/rsyslog.pid /var/run/rsyslogd.pid /var/run/mysqld/mysqld.pid /var/run/crond.pid 2>/dev/null 2>/dev/null
 supervisord -c /etc/supervisord.conf -n
