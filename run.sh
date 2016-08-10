be4213078cd9401596206c844a4963fb13615a56
johnsmithm <moshnoi2000@gmail.com>
2016-08-10 17:58:59 +0200

    added variables

--- run.sh
+++ run.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-docker build -t v1.1 .
+PORT=8003
+PWD=psw
+USER=root
+#dev1,dev2,life,stage
+DB=dev1
+HOST=1.1.1.1
+NAME=v1.1
 
-docker stop v1.1
-docker rm v1.1
-#docker run -td -p 8009:80 -v /tmp/data:/var/www/html  --name  v1.1 v1.1
-docker run -td -p 8003:80 -e "MYSQL_ROOT_PASSWORD=psw"  -e "MYSQL_USER=username"  -e "MYSQL_DATABASE=db"  -e "MYSQL_HOST=host"  -v /tmp/data:/var/www/html --name v1.1 v1.1 
-docker logs -f v1.1
-#docker run -ti v1.1 /bin/bash
+if [ "${DB}" = "dev1" ]; then
+	PORT=8003
+elif [[ "${DB}" = "dev2" ]]; then
+	PORT=8005
+elif [[ "${DB}" = "stage" ]]; then
+	PORT=8002
+elif [[ "${DB}" = "life" ]]; then
+	PORT=8006
+else 
+	PORT=8003
+fi
+
+docker build -t ${NAME} .
+
+docker stop ${NAME}
+docker rm ${NAME}
+
+docker run -td -p ${PORT}:80 -e "MYSQL_ROOT_PASSWORD=${PWD}"  -e "MYSQL_USER=${USER}" \
+  -e "MYSQL_DATABASE=${DB}"  -e "MYSQL_HOST=${HOST}"  -v /tmp/${DB}:/var/www/html --name ${NAME} ${NAME}
+
+docker logs -f ${NAME}
+
+#docker run -ti ${NAME} /bin/bash
